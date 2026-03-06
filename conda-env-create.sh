fb3b950c3f94e7ed830fa5c96b6a9285980b116b
milos7250 <56844787+milos7250@users.noreply.github.com>
2026-03-06 14:24:03 +0000

    Add dev requirements files and pytest configuration

--- conda-env-create.sh
+++ conda-env-create.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
+
+source ~/.bashrc # to ensure conda is available
+
 set -euo pipefail
 
 conda env create -f ./environment.yml
 conda activate groundgan
+
+# Dependencies need to be installed in two phases, as sparselinear needs to be installed after torch 
+pip install -r requirements.txt --extra-index-url https://download.pytorch.org/whl/cpu torch==2.9.1
+pip install -r requirements2.txt --no-build-isolation
+
 patch "$(dirname "$(which python)")/../lib/python3.11/site-packages/arboreto/core.py" ./arboreto.patch
+
+# Ask user if they want to install developer dependencies, default to no
+read -p "Do you want to install developer dependencies? (y/N) " -n 1 -r
+echo    # move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    pip install -r requirements-dev.txt
+fi
